a6e24408d0e777c35010bd73ac87aae8c7d6c38d
SandraCarretero <sandracarretero24@gmail.com>
2024-02-16 09:08:21 +0100

    change lenguage

--- src/js/index.js
+++ src/js/index.js
@@ -49,11 +49,11 @@ const getWeatherData = () => {
 			temperatureElement.textContent = Math.ceil(data.main.temp);
 			descriptionElement.textContent = data.weather[0].description;
 			humidityElement.textContent = `${data.main.humidity}%`;
-			windSpeedElement.textContent = `${data.wind.speed} Km/h`;
+			windSpeedElement.textContent = `${ Math.ceil(data.wind.speed)} Km/h`;
             maxTemperatureElement.textContent = `${Math.ceil(data.main.temp_max)}°C`
             minTemperatureElement.textContent = `${Math.ceil(data.main.temp_min)}°C`
             if (data.rain && data.rain['1h']) {
-                const precipitation = data.rain['1h']; 
+                const precipitation = Math.ceil(data.rain['1h']); 
                 precipitationElement.textContent = `${precipitation} mm`;
             } else {
                 precipitationElement.textContent = '0mm';
